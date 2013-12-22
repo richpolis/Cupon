8a2474fe348a2602478c395a6e04db2c41955ecf
richpolis <richpolis@ralcanta2>
2013-12-21 19:35:22 -0600

    Del capitulo 1 al 7

---
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Cupon\CiudadBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class DefaultController extends Controller
+{
+    public function cambiarAction($ciudad)
+    {
+        return new RedirectResponse($this->generateUrl(
+            'portada',
+            array('ciudad' => $ciudad)
+        ));
+    }
+    
+    /**
+     * Busca todas las ciudades disponibles en la base de datos y pasa la lista
+     * a una plantilla muy sencilla que simplemente muestra una lista desplegable
+     * para seleccionar la ciudad activa.
+     *
+     * @param string $ciudad El slug de la ciudad seleccionada
+     */
+    public function listaCiudadesAction($ciudad)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ciudades = $em->getRepository('CiudadBundle:Ciudad')->findAll();
+ 
+        return $this->render('CiudadBundle:Default:listaCiudades.html.twig', array(
+            'ciudadActual' => $ciudad,
+            'ciudades'     => $ciudades,
+        ));
+    }
+    
+    public function recientesAction($ciudad)
+    {
+        $em = $this->getDoctrine()->getManager();
+ 
+        $ciudad = $em->getRepository('CiudadBundle:Ciudad')
+                     ->findOneBySlug($ciudad);
+        $cercanas = $em->getRepository('CiudadBundle:Ciudad')
+                       ->findCercanas($ciudad->getId());
+        $ofertas  = $em->getRepository('OfertaBundle:Oferta')
+                       ->findRecientes($ciudad->getId());
+ 
+        return $this->render('CiudadBundle:Default:recientes.html.twig',
+            array(
+                'ciudad'   => $ciudad,
+                'cercanas' => $cercanas,
+                'ofertas'  => $ofertas
+            )
+        );
+    }
+}
---
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Cupon\OfertaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function portadaAction($ciudad)
+    {
+        $em = $this->getDoctrine()->getManager();
+ 
+        $oferta = $em->getRepository('OfertaBundle:Oferta')
+                ->findOfertaDelDia($ciudad);
+ 
+        if (!$oferta) {
+            throw $this->createNotFoundException(
+                'No se ha encontrado la oferta del día'
+            );
+        }
+ 
+        return $this->render('OfertaBundle:Default:portada.html.twig', array(
+            'oferta' => $oferta,
+            'ciudad' => $ciudad,
+        ));
+    }
+    
+    public function ayudaAction()
+    {
+        return new Response('Ayuda');
+    }
+    
+    public function ofertaAction($ciudad, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $oferta = $em->getRepository('OfertaBundle:Oferta')
+                     ->findOferta($ciudad, $slug);
+        $relacionadas = $em->getRepository('OfertaBundle:Oferta')
+                           ->findRelacionadas($ciudad);
+ 
+        return $this->render('OfertaBundle:Default:detalle.html.twig', array(
+            'oferta'       => $oferta,
+            'relacionadas' => $relacionadas
+        ));
+    }
+}
