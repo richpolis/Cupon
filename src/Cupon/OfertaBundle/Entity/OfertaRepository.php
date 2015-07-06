4684f2de11c52d725842405754723920f69ce919
Richpolis Systems <richpolis@gmail.com>
2015-07-05 21:30:09 -0500

    Agregando comando de consola (capitulo 18)

--- src/Cupon/OfertaBundle/Entity/OfertaRepository.php
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -85,4 +85,22 @@ class OfertaRepository extends EntityRepository
  
         return $consulta->getResult();
     }
+    
+    public function findOfertaDelDiaSiguiente($ciudad)
+    {
+        $em = $this->getEntityManager();
+ 
+        $consulta = $em->createQuery('
+            SELECT o, c, t
+              FROM OfertaBundle:Oferta o JOIN o.ciudad c JOIN o.tienda t
+             WHERE o.revisada = true
+               AND o.fecha_publicacion < :fecha
+               AND c.slug = :ciudad
+          ORDER BY o.fecha_publicacion DESC');
+        $consulta->setParameter('fecha', new \DateTime('tomorrow'));
+        $consulta->setParameter('ciudad', $ciudad);
+        $consulta->setMaxResults(1);
+ 
+        return $consulta->getSingleResult();
+    }
 }
